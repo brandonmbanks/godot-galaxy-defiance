8ecca6d81e520b3702896a502b3eff9031f7e354
Brandon Banks <10030196+brandonmbanks@users.noreply.github.com>
2023-11-23 10:47:39 -0500

    add scale and flash on laser spawn

--- projectiles/laser.gd
+++ projectiles/laser.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var flash_component: FlashComponent = $FlashComponent as FlashComponent
 
 func _ready() -> void:
+	# make larger when created
+	scale_component.tween_scale()
+	# flash on spawn
+	flash_component.flash()
 	# when the lasers go off the screen we need to remove them from memory
 	# we are can use the VisibleOnScreenNotifier2D to get a signal when they
 	# go off screen
